28f07bc24d2b47539b22d7a957ccb5ef38d7bf03
KoiiroHikanyan <hikanyan0131@gmail.com>
2024-07-17 20:00:13 +0900

    [Update]

---
+++ Source/UE5CppNetworkGame/Public/Tank/Tank.h
@@ -0,0 +1,39 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BasePawn.h"
+#include "EnhancedActionKeyMapping.h"
+
+#include "Tank.generated.h"
+
+class UCameraComponent;
+class USpringArmComponent;
+/**
+ * 
+ */
+UCLASS()
+class UE5CPPNETWORKGAME_API ATank : public ABasePawn
+{
+	GENERATED_BODY()
+
+public:
+	ATank();
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+	virtual void BeginPlay();
+
+private:
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+	USpringArmComponent* SpringArm;
+
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+	UCameraComponent* Camera;
+
+	UPROPERTY(EditAnywhere, Category = "Input")
+	TSoftObjectPtr<UInputMappingContext> InputMapping;
+	UPROPERTY(EditAnywhere, Category = "Input")
+	class UInputAction* MoveAction;
+
+	void PressedAxis(const FInputActionValue& Value);
+};
